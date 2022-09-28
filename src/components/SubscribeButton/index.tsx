8a5a33e7cae5c90de4f98a6c6b948781007ce283
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-09-28 16:08:32 -0300

    configure stripe

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.scss";
+
+export function SubscribeButton() {
+  return <button className={styles.subscribeButton}>Susbscribe now</button>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "../styles/pages/home.module.scss";
 
@@ -10,7 +11,24 @@ export default function Home() {
         <meta name="description" content="Ignews by Rocketseat" />
       </Head>
 
-      <div className={styles.homeContainer}></div>
+      <main className={styles.container}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/assets/avatar.svg" alt="" className={styles.hero} />
+      </main>
     </>
   );
 }
